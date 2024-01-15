ca752a30ccfae3a84dc49dc0931432dc2a480028
SujalVerman <152647296+SujalVerman@users.noreply.github.com>
2024-01-15 22:48:42 +0530

    Add files via upload

---
+++ day8/array.js
@@ -0,0 +1,14 @@
+// let na = [1,2,3,4,5,6,7,8,9]
+// console.log(na[0])
+// console.log(na[1])
+// console.log(na[2])
+// console.log(na[3])
+// console.log(na[4])
+// console.log(na[5])
+
+// Quick Quiz
+
+const a=[1,2,3,4,5,6,7]
+for(let i=0;i<(a.length);i++){
+    console.log(a[i])
+}
---
+++ day8/practice_set.js
@@ -0,0 +1,49 @@
+"use strict";
+const ps = require("prompt-sync");
+const prompt= ps();
+
+//Ques 1
+// let a= "harr\""
+// console.log(a.length)
+
+//Ques 2
+// const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+// const word = 'fox';
+
+// console.log( `The word "${word}" ${sentence.includes(word) ? 'is' : 'is not'} in the sentence`,)
+
+// const str1 = 'Saturday night plans';
+
+// console.log(str1.startsWith('Sat'));
+
+// console.log(str1.startsWith('Sat', 3));
+
+// const str1 = 'Cats are the best!';
+
+// console.log(str1.endsWith('best!'));
+// Expected output: true
+
+// console.log(str1.endsWith('are', 8));
+// Expected output: true
+
+// const str2 = 'Is this a question?';
+
+// console.log(str2.endsWith('question'));
+// Expected output: false
+
+
+//Ques 3
+// const s= "SUJal"
+// console.log(s.toLocaleLowerCase(s))
+
+//Ques 4
+// let extract= "please give RS 1000"
+// let bh=Number.parseInt(console.log(extract.slice("please give RS ".length)))
+// console.log(typeof(bh))
+
+//Ques 5
+// let y= "Ishanvi"
+
+// y[3]="s"// we can't change the string coz it is immutable
+// console.log(y)
