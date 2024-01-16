9c1becd88343024c3daca92181ebb505a211975e
SujalVerman <152647296+SujalVerman@users.noreply.github.com>
2024-01-16 19:05:07 +0530

    Add files via upload

---
+++ day9/arr_methods.js
@@ -0,0 +1,19 @@
+let arr=[1,2,3,56,7]
+// console.log(arr.toString()) //converted into string
+
+// console.log(arr.pop()) //remove end of the element
+
+// console.log(arr.join("-")) //add a specific elements between array or in object
+
+// console.log(arr.push(9)) // add in end of the element
+// console.log(arr)
+
+// console.log(arr.shift()) // remove the first element
+// console.log(arr)
+
+// console.log(arr.unshift(9)) //add element in begining
+// console.log(arr)
+
+let arr1= [5,6,8]
+let arr2= [9,10,11]
+console.log(arr.concat(arr1,arr2)) //join the arrays 
