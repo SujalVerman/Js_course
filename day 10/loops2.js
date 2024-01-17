9267ed0ab4bb07333e38596305d01712563dfc98
SujalVerman <152647296+SujalVerman@users.noreply.github.com>
2024-01-17 16:04:21 +0530

    Add files via upload

---
+++ day 10/loops2.js
@@ -0,0 +1,18 @@
+// let num=[1,2,3,4,55,34,23,12]
+// num.forEach(element => {         // forEach loop
+//     console.log(element*element) // it will print the square of this array
+// });
+
+// let n ="harry"
+// let arr= Array.from(n)  // it will form an array
+// console.log(arr) 
+
+// let num=[1,2,3,4,55,34,23,12]
+// for(let i of num)   // it is the easy way to access the { for loop }
+// console.log(i)   
+
+let num=[1,2,3,4,55,34,23,12]
+for(let i in num)
+console.log(num[i]) // this will print the for loop like the { for...of loop }
+console.log(i) // this will print the keys of the array 
+
