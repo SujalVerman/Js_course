ba8b541872e61dd72bb4b18a7671f288d571bafd
SujalVerman <152647296+SujalVerman@users.noreply.github.com>
2024-01-17 15:56:05 +0530

    Add files via upload

---
+++ day10/arr_methods2.js
@@ -0,0 +1,19 @@
+// let compare=(a,b)=>{
+//     return a-b
+// }                               // if we comment out this compare function then it will sort according to alphabaticaly
+
+// let num= [1,2,3,44,55,23,45,21]
+// console.log(num.sort(compare))  // it will sort according to assending order
+
+// let num= [1,2,3,44,55,23,45,21]
+// let deletedValues=num.splice(2,3,12,344,566,666) // i will add new items to an array
+// console.log(deletedValues)
+// console.log(num)
+
+// let num= [1,2,3,44,55,23,45,21]
+// console.log(num.slice(2)) // this will delete a particular item and rest will print
+// console.log(num.slice(1,3)) // this will print from __ to __ and rest will delete
+
+let num= [1,2,3,44,55,23,45,21]
+num.reverse()
+console.log(num) // it reversed the array
